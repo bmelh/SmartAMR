c60e9376adaf622ebdf9f39a96e8f2214801e9ac
Byoko295 <mohamedelhabib.benouis@hotmail.com>
2025-09-16 08:53:35 +0200

    Add files via upload

---
+++ SmartAMR/module_checker.py
@@ -0,0 +1,24 @@
+import numpy as np
+import scipy
+import pandas as pd
+import tensorflow as tf
+import keras
+import sklearn
+import seaborn
+import xgboost
+import torch
+import Bio
+
+print(f"Numpy version: {np.__version__}")
+print(f"Scipy version: {scipy.__version__}")
+print(f"pandas version: {pd.__version__}")
+print(f"tensorflow version: {tf.__version__}")
+print(f"keras version: {keras.__version__}")
+print(f"Biopython version: {Bio.__version__}")
+print(f"Sci-kit learn version: {sklearn.__version__}")
+print(f"Seaborn version: {seaborn.__version__}")
+print(f"XGBoost version: {xgboost.__version__}")
+print(f"Pytorch version: {torch.__version__}")
+
+
+

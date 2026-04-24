86be5f800fba2bd46c9732f51c8d68816cf9fda4
root <root@Habib.localdomain>
2026-04-24 16:45:37 +0200

    Old root file deletion

--- SmartAMR/scripts/embedding.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Generate embeddings for proteins present in a FASTA file
-esm-extract esm2_t6_8M_UR50D
--- SmartAMR/scripts/filter_bacterian_proteomes.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-for file in ../tmp/pep/*.faa; do
-    awk -f scripts/extract_proteins.awk ./config/selected_bacterian_proteins.txt "${file}" > "${file/.faa/.selected.faa}"
-done
